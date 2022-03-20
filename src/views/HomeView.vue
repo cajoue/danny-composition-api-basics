df28ffaf20d278ad7aa5b8cb629ae0c4284a8f83
cajoue <cajoue@users.noreply.github.com>
2022-03-20 14:40:03 +0100

    Install tailwindcss

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="text-3xl font-semibold">
+    Home
+  </h1>
 </template>
