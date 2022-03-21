4021b448dc7abc6c64bc34cf25546e07e2bfbbf8
cajoue <cajoue@users.noreply.github.com>
2022-03-21 19:21:17 +0100

    options api basic counter

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,32 @@
 <template>
-  <h1 class="text-3xl font-semibold">
-    Home
-  </h1>
+  <div class="flex space-x-6 items-baseline">
+    <button
+        class="px-4 py-2 bg-emerald-100 border-2 border-emerald-300 rounded"
+        @click="decreaseCounter">-
+    </button>
+    <span class="text 2xl font-semibold">{{ counter }}</span>
+    <button
+        class="px-4 py-2 bg-emerald-100 border-2 border-emerald-300 rounded"
+        @click="increaseCounter">+
+    </button>
+  </div>
 </template>
+
+<!-- options api pattern-->
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increaseCounter() {
+      this.counter++
+    },
+    decreaseCounter() {
+      this.counter--
+    }
+  }
+}
+</script>
